1c361418795c96df7ddd2005bbc23d347c8f1229
Kostas <kostas.chaniotakis@gmail.com>
2021-09-07 18:27:59 +0200

    users.role created

--- deskbookingsystem42/users/models.py
+++ deskbookingsystem42/users/models.py
@@ -1,8 +1,13 @@
+from django.contrib.auth import models
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db.models import CharField, Model, ForeignKey
+from django.db.models.deletion import SET_NULL
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
+class Role(Model):
+    """Default role for 42users."""
+    name = CharField(max_length=200, blank=True, default=False)
 
 class User(AbstractUser):
     """Default user for DeskBookingSystem42."""
@@ -11,6 +16,8 @@ class User(AbstractUser):
     name = CharField(_("Name of User"), blank=True, max_length=255)
     first_name = None  # type: ignore
     last_name = None  # type: ignore
+    role = ForeignKey(Role, null=True, blank=True, on_delete=SET_NULL)
+    school = None #@Philipp  
 
     def get_absolute_url(self):
         """Get url for user's detail view.
