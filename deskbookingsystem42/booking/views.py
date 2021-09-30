bb032148fce9f36ac3ac8e57a59783a394d5c17c
unknown <latrache.abdel.fr@gmail.com>
2021-09-30 18:50:30 +0200

    navbar

--- deskbookingsystem42/booking/views.py
+++ deskbookingsystem42/booking/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from allauth.account.forms import LoginForm, SignupForm
 
 # Create your views here.
 class BookingView(TemplateView):
     template_name= "booking/booking-template.html"
+    context= {
+        'LoginForm': LoginForm(),
+        'SignupForm': SignupForm()
+    }
 
     # def add_booking:
 
